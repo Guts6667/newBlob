be9b66b8880e60aedc5bd7c41601204fb9c1a6ba
Guts6667 <76847062+Guts6667@users.noreply.github.com>
2023-10-20 16:55:42 +0200

    V.0.4.3
    Issue Fixed ✅
    Header ✅
    Menu ✅
    Hero ✅
    Selected Works ✅
    Works ✅
    Footer ✅
    Services ✅
    WorkPage (Wip)
    Services (WIP)
    Rebuilt with Tailwind CSS
    Need to reduce assets sizes
    Added Styled jsx

--- src/components/(Home)/ServicesHome/index.jsx
+++ src/components/(Home)/ServicesHome/index.jsx
@@ -7,7 +7,7 @@ const services = datas.default
 console.log(services)
   return (
     <section className='flex flex-col gap-50'>
-        <h2>Services</h2>
+        <h2 className='text-[40px] md:text-[72px] !important'>Services</h2>
         <div className='flex flex-wrap gap-25 '>
             {services.map((service) => (
                <CardService key={service.id}  service={service} />
