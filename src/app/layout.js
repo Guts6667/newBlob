1b85f864d94f34ca49449800c9c1142aaf46bffe
Guts6667 <76847062+Guts6667@users.noreply.github.com>
2023-10-22 23:40:44 +0200

    V.0.8.5
    Changed meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ export const metadata = {
   title: "BLOB-Agency",
   description:
     "BLOB-Agency is an international digital agency known for its expertise in creative design, website development, product management, and application development. With a proven track record of successful projects and a dedicated team of professionals, we are committed to delivering exceptional digital solutions for businesses. Collaborate with us for innovative and effective digital strategies",
+
 };
 
 export default function RootLayout({ children }) {
@@ -16,6 +17,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="42x70" href="/favicon.png"/>
+          <link rel="icon" type="image/png" sizes="10x17" href="/favicon.png"/>
       </head>
       <body className={inter.className}>
         <Header />
