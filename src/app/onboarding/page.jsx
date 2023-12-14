945c2fd9c361fa4ba984c74c1591ae44f92cc8a9
Guts6667 <76847062+Guts6667@users.noreply.github.com>
2023-12-14 07:04:14 +0100

    V.1.2.1

---
+++ src/app/onboarding/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Onbarding() {
+  return (
+    <main className="mt-[150px] mb-[100px] flex flex-col gap-50 px-25 md:px-50">
+      <h1>Onboarding Page coming soon... 👀</h1>
+    </main>
+  );
+}
